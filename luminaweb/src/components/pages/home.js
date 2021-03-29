21783a3a5234124441c7cb5acc91cfa8e7888891
TristanDCP <tristandacostapinto@gmail.com>
2021-03-29 15:04:18 +0200

    debut de nav

---
+++ luminaweb/src/components/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Page d'accueil mon frère</h1>
+    </div>
+  );
+};
+  
+export default Home;
